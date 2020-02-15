6312894dd198607bd0c149be3d948f2655c2b7dd
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-15 16:32:56 +0400

    created Cats class

---
+++ src/Collections/Cats.java
@@ -0,0 +1,47 @@
+package Collections;
+
+
+public class Cats {
+
+    //fields
+    private String breed;
+    private int avgWeight;
+    private int age;
+
+    //constructor
+    public Cats(String breed, int avgWeight, int age) {
+        this.breed = breed;
+        this.avgWeight = avgWeight;
+        this.age = age;
+    }
+
+    //getters and setters
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAvgWeight() {
+        return avgWeight;
+    }
+
+    public void setAvgWeight(int avgWeight) {
+        this.avgWeight = avgWeight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cats" + "\n" + "breed: " + breed + '\n' + "avgWeight: " + avgWeight + "\n" + "age: " + age;
+    }
+}
---
+++ src/Collections/Main.java
@@ -0,0 +1,9 @@
+package Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*** Arrays ***");
+
+        System.out.println("*** ArrayList ***");
+    }
+}
