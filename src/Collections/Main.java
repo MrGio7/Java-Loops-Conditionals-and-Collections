eaefe16c3f6962f0b75094b27d22be91eca29fc6
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-15 16:40:43 +0400

    added cats array

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -2,8 +2,23 @@ package Collections;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("*** Arrays ***");
 
+        Cats[] catArr = new Cats[3];
+        catArr[0] = new Cats("Egyptian", 10, 5);
+        catArr[2] = new Cats("Arctic", 15, 7);
+
+        //For Loop
+        for (int i = 0; i < catArr.length; i++) {
+            System.out.println(catArr[i]);
+        }
+
+        System.out.println();
+
         System.out.println("*** ArrayList ***");
+
+        System.out.println("*** HashMaps ***");
+
     }
 }
